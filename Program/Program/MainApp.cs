e73104dcde6d075369322519c6ed1365b7854bed
Alexey <alexey.karm@mail.ru>
2019-12-30 09:41:59 +0300

    Creating game project with class.

---
+++ Program/Program/MainApp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
