61a0af80ee805c47883aa45f37b32c0117b97b2f
Alexey <alexey.karm@mail.ru>
2020-01-06 03:04:25 +0300

    Adding a new object "Pacman"

--- Program/Program/Program.cs
+++ Program/Program/Program.cs
@@ -12,13 +12,13 @@ namespace Program
     {
         static void Main(string[] args)
         {
-             Engine.Run(new IGameObject[] { }); //при загрузке передаем новый обьект для отображения и обработки
+            Engine.Run(new IGameObject[] { new Pacman { } }); //при загрузке передаем новый обьект для отображения и обработки
         }
 
         abstract class BaseGameObject : IGameObject
         {
             public string Name { get; set; }
-            public bool IsEnabled { get; set; } = true; //по умолчанию включен
+            public bool IsEnabled { get; set; } //по умолчанию включен
             public Animation Animation { get; set; }
             public abstract void Update();
         }
@@ -27,52 +27,32 @@ namespace Program
         {
             public Pacman()    //конструктор класса
             {
-                Animation = AnimationFactory.CreateAnimation(AnimationType.PacmanLeft);
-            }
+                Name = "User";
 
-            //Animation.Location = new Coordinate(1, 2);
+                IsEnabled = true;
+                
+                Animation = AnimationFactory.CreateAnimation(AnimationType.PacmanRight);
+
+            }
 
             public DirectionKeys PressedKeys { get; set; }
 
             public void Collide(IEnumerable<IGameObject> collisions)
             {
-                bool rightKeyPressed = (PressedKeys & DirectionKeys.Right) == DirectionKeys.Right;
-
-                var StepRight = new Coordinate(0.001f, 0);      //создадим обьект для смещения вправо на юнит
-
-                if (rightKeyPressed)
-                    Animation.Location += StepRight;     //прибавяем один юнит по x = сдвиг вправо
-
+               
             }
-
 
             public override void Update()
             {
+                Animation.Location = new Coordinate(10, 10);    //стартовая позиция юзера
 
+                bool rightKeyPressed = (PressedKeys & DirectionKeys.Right) == DirectionKeys.Right;
+
+                var StepRight = new Coordinate(0.09f, 0);      //создадим обьект для смещения вправо на юнит
+
+                if (rightKeyPressed)
+                    Animation.Location += StepRight;     //прибавяем один юнит по x = сдвиг вправо
             }
         }
-
-
-        //class Pacman : IProtagonist
-        //{
-        //    DirectionKeys IProtagonist.PressedKeys { set => throw new NotImplementedException(); }
-
-        //    string IGameObject.Name => throw new NotImplementedException();
-
-        //    bool IGameObject.IsEnabled { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-
-        //    Animation IGameObject.Animation => throw new NotImplementedException();
-
-        //    void IProtagonist.Collide(IEnumerable<IGameObject> collisions)
-        //    {
-        //        throw new NotImplementedException();
-        //    }
-
-        //    void IGameObject.Update()
-        //    {
-        //        throw new NotImplementedException();
-        //    }
-        //}
-
     }
 }
