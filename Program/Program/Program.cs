5b998dae25f03215ea3e350d6892a225c1940cb5
Alexey <alexey.karm@mail.ru>
2020-01-07 00:02:02 +0300

    Add animation for Pacman & Create Maze

--- Program/Program/Program.cs
+++ Program/Program/Program.cs
@@ -12,7 +12,11 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            Engine.Run(new IGameObject[] { new Pacman { } }); //при загрузке передаем новый обьект для отображения и обработки
+
+            Pacman mainPacman = new Pacman();
+            MazeBlue maze = new MazeBlue();
+
+            Engine.Run(new IGameObject[] { mainPacman, maze }); //при загрузке передаем новый обьект для отображения и обработки
         }
 
         abstract class BaseGameObject : IGameObject
@@ -27,12 +31,12 @@ namespace Program
         {
             public Pacman()    //конструктор класса
             {
-                Name = "User";
+                Name = "Pacman";
 
                 IsEnabled = true;
                 
                 Animation = AnimationFactory.CreateAnimation(AnimationType.PacmanRight);
-
+                Animation.Location = new Coordinate(10, 10);    //стартовая позиция юзера
             }
 
             public DirectionKeys PressedKeys { get; set; }
@@ -44,14 +48,52 @@ namespace Program
 
             public override void Update()
             {
-                Animation.Location = new Coordinate(10, 10);    //стартовая позиция юзера
-
                 bool rightKeyPressed = (PressedKeys & DirectionKeys.Right) == DirectionKeys.Right;
+                bool leftKeyPressed = (PressedKeys & DirectionKeys.Left) == DirectionKeys.Left;
+                bool upKeyPressed = (PressedKeys & DirectionKeys.Up) == DirectionKeys.Up;
+                bool downKeyPressed = (PressedKeys & DirectionKeys.Down) == DirectionKeys.Down;
+
 
                 var StepRight = new Coordinate(0.09f, 0);      //создадим обьект для смещения вправо на юнит
+                var StepLeft = new Coordinate(-0.09f, 0);      
+                var StepUp = new Coordinate(0, -0.09f);      
+                var StepDown = new Coordinate(0, 0.09f);
 
                 if (rightKeyPressed)
-                    Animation.Location += StepRight;     //прибавяем один юнит по x = сдвиг вправо
+                {
+                    Animation.Location += StepRight;     //прибавляем один юнит по x = сдвиг вправо
+                    //Animation = AnimationFactory.CreateAnimation(AnimationType.PacmanRight);
+                }
+                else if (leftKeyPressed)
+                {
+                    Animation.Location += StepLeft;
+                    //Animation = AnimationFactory.CreateAnimation(AnimationType.PacmanLeft);
+                }
+                else if (upKeyPressed)
+                {
+                    Animation.Location += StepUp;
+                   // Animation = AnimationFactory.CreateAnimation(AnimationType.PacmanUp);
+                }
+                else if (downKeyPressed)
+                {
+                    Animation.Location += StepDown;
+                    //Animation = AnimationFactory.CreateAnimation(AnimationType.PacmanDown);
+                }
+            }
+        }
+
+        class MazeBlue : BaseGameObject
+        {
+            public MazeBlue()
+            {
+                Name = "MazeBlue";
+                IsEnabled = true;
+                Animation = AnimationFactory.CreateAnimation(AnimationType.MazeBlue);
+            }
+
+            public override void Update()
+            {
+                Animation.Location = new Coordinate(0, 0);    //стартовая позиция поля
             }
         }
     }
