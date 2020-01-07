cf08512dc3af4436a828ffde21540f7275be9ddb
Alexey <alexey.karm@mail.ru>
2020-01-07 22:22:37 +0300

    Add new class "Coin", put some animation for Pacman

--- Program/Program/Program.cs
+++ Program/Program/Program.cs
@@ -14,9 +14,12 @@ namespace Program
         {
 
             Pacman mainPacman = new Pacman();
-            MazeBlue maze = new MazeBlue();
 
-            Engine.Run(new IGameObject[] { mainPacman, maze }); //при загрузке передаем новый обьект для отображения и обработки
+            MazeBlue mazeBlue = new MazeBlue();
+
+            SmallCoin coin = new SmallCoin();
+
+            Engine.Run(new IGameObject[] { mainPacman, mazeBlue, coin}); //при загрузке передаем обьекты для отображения и обработки
         }
 
         abstract class BaseGameObject : IGameObject
@@ -29,6 +32,9 @@ namespace Program
 
         class Pacman : BaseGameObject, IProtagonist
         {
+            public DirectionKeys PressedKeys { get; set; }
+            
+
             public Pacman()    //конструктор класса
             {
                 Name = "Pacman";
@@ -36,10 +42,11 @@ namespace Program
                 IsEnabled = true;
                 
                 Animation = AnimationFactory.CreateAnimation(AnimationType.PacmanRight);
-                Animation.Location = new Coordinate(10, 10);    //стартовая позиция юзера
+
+                Animation.Location = new Coordinate(10, 10);    //стартовая позиция пакмана
             }
 
-            public DirectionKeys PressedKeys { get; set; }
+            
 
             public void Collide(IEnumerable<IGameObject> collisions)
             {
@@ -53,7 +60,6 @@ namespace Program
                 bool upKeyPressed = (PressedKeys & DirectionKeys.Up) == DirectionKeys.Up;
                 bool downKeyPressed = (PressedKeys & DirectionKeys.Down) == DirectionKeys.Down;
 
-
                 var StepRight = new Coordinate(0.09f, 0);      //создадим обьект для смещения вправо на юнит
                 var StepLeft = new Coordinate(-0.09f, 0);      
                 var StepUp = new Coordinate(0, -0.09f);      
@@ -62,22 +68,30 @@ namespace Program
                 if (rightKeyPressed)
                 {
                     Animation.Location += StepRight;     //прибавляем один юнит по x = сдвиг вправо
-                    //Animation = AnimationFactory.CreateAnimation(AnimationType.PacmanRight);
+                    Coordinate anim = Animation.Location;
+                    Animation = AnimationFactory.CreateAnimation(AnimationType.PacmanRight);
+                    Animation.Location = anim;
                 }
                 else if (leftKeyPressed)
                 {
                     Animation.Location += StepLeft;
-                    //Animation = AnimationFactory.CreateAnimation(AnimationType.PacmanLeft);
+                    Coordinate anim = Animation.Location;
+                    Animation = AnimationFactory.CreateAnimation(AnimationType.PacmanLeft);
+                    Animation.Location = anim;
                 }
                 else if (upKeyPressed)
                 {
                     Animation.Location += StepUp;
-                   // Animation = AnimationFactory.CreateAnimation(AnimationType.PacmanUp);
+                    Coordinate anim = Animation.Location;
+                    Animation = AnimationFactory.CreateAnimation(AnimationType.PacmanUp);
+                    Animation.Location = anim;
                 }
                 else if (downKeyPressed)
                 {
                     Animation.Location += StepDown;
-                    //Animation = AnimationFactory.CreateAnimation(AnimationType.PacmanDown);
+                    Coordinate anim = Animation.Location;
+                    Animation = AnimationFactory.CreateAnimation(AnimationType.PacmanDown);
+                    Animation.Location = anim;
                 }
             }
         }
@@ -87,14 +101,44 @@ namespace Program
             public MazeBlue()
             {
                 Name = "MazeBlue";
+
                 IsEnabled = true;
+
                 Animation = AnimationFactory.CreateAnimation(AnimationType.MazeBlue);
+
+                Animation.Location = new Coordinate(0, 0);    //стартовая позиция поля
             }
 
             public override void Update()
             {
-                Animation.Location = new Coordinate(0, 0);    //стартовая позиция поля
+
             }
         }
+
+        class SmallCoin : BaseGameObject
+        {
+            public SmallCoin()
+            {
+                Name = "SmallCoin";
+
+                IsEnabled = true;
+
+                Animation = AnimationFactory.CreateAnimation(AnimationType.SmallCoin);
+
+                //рандомизируем появление монеток
+                Random rnd = new Random();
+
+                int coinX = rnd.Next(0, 21);
+                int coinY = rnd.Next(0, 27);
+
+                Animation.Location = new Coordinate(coinX, coinY);    //стартовая позиция монетки
+            }
+
+            public override void Update()
+            {
+                
+            }
+        }
+
     }
 }
